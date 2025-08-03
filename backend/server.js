91924742ecf6b3e6509b63f8ff719241145ac4b3
avi-i05 <118112869+avi-i05@users.noreply.github.com>
2025-08-03 12:01:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ const historyRoutes = require("./routes/historyRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://eap-frontend-neon.vercel.app',
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use(
